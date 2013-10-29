8068fbad395e759530079a7568066979e59bc662
Trude <trude.martinsen@gmail.com>
2013-10-29 11:04:33 +0100

    Henter aktivitetene fra js-fil med angular

---
+++ js/activities.js
@@ -0,0 +1,10 @@
+function Ctrl($scope) {
+  $scope.activities = [
+      {name:'Ola Nordmann', date:'23. okt 2013 kl. 20:37', pic:'olaNordmann', type:'syklet', distance:'40km', time:'5 timer', points:65, kudos:3, comments:5},
+      {name:'Bjørn Dæhli', date:'24. okt 2013 kl. 10:00', pic:'profilePicture2', type:'jogget', distance:'5km', time:'40 minutter', points:30, kudos:1, comments:15},
+      {name:'Kari Nordmann', date:'25. okt 2013 kl. 11:07', pic:'profilePicture3', type:'løpt', distance:'10km', time:'1 time', points:43, kudos:5, comments:1},
+      {name:'Kjell Rusti', date:'19. sept 2013 kl. 22:41', pic:'kjellRusti', type:'gått på ski', distance:'20km', time:'1 time', points:39, kudos:12, comments:9},
+      {name:'Marit Bjørgen', date:'2. aug 2013 kl. 19:49', pic:'maritBjorgen', type:'syklet', distance:'50km', time:'3 timer', points:72, kudos:33, comments:1},
+      {name:'Endaett Navn', date:'29. okt 2013 kl. 14:40', pic:'profilePicture', type:'gått', distance:'3km', time:'5 timer', points:14, kudos:1, comments:4}
+    ];
+}
