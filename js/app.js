014465a88b2a252634bd4c2193971b7c8203cde9
Trude <trude.martinsen@gmail.com>
2013-10-31 20:16:10 +0100

    Oppdatert index.html med det den skal inneholder, og fikset opp i innhold i home.html

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 angular.module('teamsteria', []).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.
-            when('/home', {templateUrl: 'partials/home.html',   controller: 'KalkyleMenyCtrl'}).
+            when('/home', {templateUrl: 'partials/home.html',   controller: 'Ctrl'}).
             when('/menu', {templateUrl: 'partials/menu.html',   controller: 'DokumentCtrl'}).
             when('/pageone/:someId', {templateUrl: 'partials/pageWithId.html', controller: 'PageWithIdCtrl'}).
             otherwise({redirectTo: '/home'});
