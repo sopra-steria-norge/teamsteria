f05c8f2404c2dccc496709061ccac262bc474d38
Ørjan Markhus Lillevik <olillevik@gmail.com>
2013-10-31 23:19:36 +0100

    adding activities page

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ angular.module('teamsteria', []).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.
             when('/home', {templateUrl: 'partials/home.html',   controller: 'HomeCtrl'}).
-            when('/register', {templateUrl: 'partials/register.html',   controller: 'MenuCtrl'}).
             when('/profile', {templateUrl: 'partials/profile.html',   controller: 'ProfileCtrl'}).
+			when('/activity', {templateUrl: 'partials/activity.html', controller: 'ActivityCtrl'}).
             when('/pageone/:someId', {templateUrl: 'partials/pageWithId.html', controller: 'PageWithIdCtrl'}).
             otherwise({redirectTo: '/home'});
     }]);
