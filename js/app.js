ed853ad20d0bad3deb0ca17eccb5c512a3785574
Trude <trude.martinsen@gmail.com>
2013-10-31 21:30:40 +0100

    Lagt til side for å registrere trening

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ angular.module('teamsteria', []).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.
             when('/home', {templateUrl: 'partials/home.html',   controller: 'HomeCtrl'}).
-            when('/menu', {templateUrl: 'partials/menu.html',   controller: 'MenuCtrl'}).
+            when('/register', {templateUrl: 'partials/register.html',   controller: 'MenuCtrl'}).
             when('/pageone/:someId', {templateUrl: 'partials/pageWithId.html', controller: 'PageWithIdCtrl'}).
             otherwise({redirectTo: '/home'});
     }]);
