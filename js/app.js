94346afb74b463529a1bac862c040d194baa6443
Trude <trude.martinsen@gmail.com>
2013-11-01 10:37:24 +0100

    Startet på registrer aktivitet siden

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ angular.module('teamsteria', []).
             when('/home', {templateUrl: 'partials/home.html',   controller: 'HomeCtrl'}).
             when('/profile', {templateUrl: 'partials/profile.html',   controller: 'ProfileCtrl'}).
 			when('/activity', {templateUrl: 'partials/activity.html', controller: 'ActivityCtrl'}).
+			when('/date', {templateUrl: 'partials/date.html', controller: 'DateCtrl'}).
+			when('/duration', {templateUrl: 'partials/duration.html', controller: 'DurationCtrl'}).
+			when('/pickActivity', {templateUrl: 'partials/pickActivity.html', controller: 'PickActivityCtrl'}).
             when('/pageone/:someId', {templateUrl: 'partials/pageWithId.html', controller: 'PageWithIdCtrl'}).
             otherwise({redirectTo: '/home'});
     }]);
